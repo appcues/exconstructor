138b5e7acf67e917d3e3639f2eea2851430b043f
pete gamache <pete@gamache.org>
2016-01-19 18:08:27 -0500

    improved docs

--- lib/exconstructor.ex
+++ lib/exconstructor.ex
@@ -3,11 +3,11 @@ defmodule ExConstructor do
   ExConstructor is an Elixir library which makes it easier to instantiate
   structs from external data, such as that emitted by a JSON parser.
 
-  It provides a `define_constructor` macro that can be invoked from struct
-  modules.  This macro defines a constructor, by default called `new`,
-  that accepts struct values as either a map or a dict, whose keys are
-  either strings, and whose keys may be formatted in `camelCase` or
-  `under_score` format (or a literal match on the struct field name).
+  ExConstructor provides a `define_constructor` macro which can be invoked
+  from a struct module.  The generated constructor, called `new` by default,
+  handles map-vs-dict, string-vs-atom, and camelCase-vs-under_score
+  input data issues automatically, DRYing up your code and letting you
+  move on to the interesting parts of your program.
 
   ## Installation
 
