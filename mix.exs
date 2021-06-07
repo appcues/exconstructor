1234304e7c53cc6490d467f63216215bcdd8b503
Nate Tenczar <ntenczar@gmail.com>
2021-06-07 17:28:12 -0400

    1.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExConstructor.Mixfile do
   def project do
     [
       app: :exconstructor,
-      version: "1.2.2",
+      version: "1.2.3",
       description: description(),
       package: package(),
       elixir: "~> 1.2",
