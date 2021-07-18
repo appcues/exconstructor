bf23f4d456a668cd8860944922aa3073a01d219f
Kian-Meng, Ang <kianmeng@cpan.org>
2021-07-18 16:34:45 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,35 +1,35 @@
 defmodule ExConstructor.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/appcues/exconstructor"
+  @version "1.2.3"
+
   def project do
     [
       app: :exconstructor,
-      version: "1.2.3",
-      description: description(),
-      package: package(),
+      version: @version,
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      docs: [main: ExConstructor],
-      test_coverage: [tool: ExCoveralls],
-      # test_coverage: [tool: Coverex.Task],
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls]
     ]
-  end
-
-  def description do
-    ~S"""
-    ExConstructor generates constructor functions for your structs, handling
-    map-vs-keyword-list, string-vs-atom-keys, and camelCase-vs-under_score
-    issues automatically.
-    """
   end
 
   def package do
     [
+      description:
+        "ExConstructor generates constructor functions for your structs, handling" <>
+          " map-vs-keyword-list, string-vs-atom-keys, and camelCase-vs-under_score" <>
+          " issues automatically.",
       maintainers: ["pete gamache", "Appcues"],
       licenses: ["MIT"],
-      links: %{GitHub: "https://github.com/appcues/exconstructor"}
+      links: %{
+        Changelog: "https://hexdocs.pm/exconstructor/changelog.html",
+        GitHub: @source_url
+      }
     ]
   end
 
@@ -41,9 +41,22 @@ defmodule ExConstructor.Mixfile do
     [
       {:ex_spec, "~> 2.0.1", only: :test},
       {:excoveralls, "~> 0.13", only: :test},
-      {:earmark, "~> 0.1", only: :dev},
-      {:ex_doc, "~> 0.24", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "CHANGELOG.md": [],
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
