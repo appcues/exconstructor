5f3265b6c2b4964b5285ff95afc954451ade3692
Rubem Nakamura <rubem.nakamura@gmail.com>
2024-12-06 16:46:55 -0300

    version 1.12.15

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExConstructor.Mixfile do
   def project do
     [
       app: :exconstructor,
-      version: "1.2.14",
+      version: "1.2.15",
       description: description(),
       package: package(),
       elixir: "~> 1.2",
